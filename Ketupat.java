6b1af0be7c30de18510978e1075191c9d55020a6
alarief <14410100061@stikom.edu>
2015-04-09 07:30:05 +0700

    Create Ketupat.java

---
+++ Ketupat.java
@@ -0,0 +1,32 @@
+import java.util.Scanner; 
+public class Ketupat { 
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in); 
+        int i; 
+        int n = sc.nextInt(); 
+ 
+        for (i = 0; i < (2 * n) + 1; i++) { 
+ 
+            for (int j = 0; j < (2 * n) + 1; j++) { 
+ 
+                if (i < n) { 
+                    if ((j > n - i) && (j < n + i)) { 
+                        System.out.print(" "); 
+                    } else { 
+                        System.out.print("*"); 
+                    } 
+                } else { 
+ 
+                    if ((j > i - n) && (j < 3*n-i)) { 
+                        System.out.print(" "); 
+                    } else { 
+                        System.out.print("*"); 
+                    } 
+ 
+                } 
+            } 
+            System.out.println(); 
+ 
+        } 
+    } 
+} 
