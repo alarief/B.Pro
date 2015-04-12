2fd55cdaf56cdd41e6945789eca5572583510903
alarief <14410100061@stikom.edu>
2015-04-12 15:21:43 +0700

    Create TabelPerkalian.java

---
+++ TabelPerkalian.java
@@ -0,0 +1,23 @@
+import  java.io.*;
+public class TabelPerkalian {
+    public static void main(String[] args)throws IOException
+    {
+   
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        int i = 1, j = 1, hasil;
+        String enter;
+
+        for (i = 1; i <= 10; i++) {
+            for (j = 1; j <= 10; j++) {
+                hasil = i * j;
+                System.out.print(i + "x" + j + '=' + hasil);
+     System.out.print("  ");
+                if (j == 10) {
+                    System.out.println();
+                   
+                }
+            }
+        }
+    }
+}
