3a487b8e7f93db56a6c39e36c9b7f9395bdc69ad
alarief <14410100061@stikom.edu>
2015-04-12 15:06:28 +0700

    Create ContohArray.java

---
+++ ContohArray.java
@@ -0,0 +1,17 @@
+public class ContohArray{
+  public static void main(String []argc){
+    int [][]data={
+      {1,0,0,0,0,0},
+      {0,1,0,0,0,0},
+      {0,0,1,0,0,0},
+      {0,0,0,1,0,0},
+      {0,0,0,0,1,1},
+      {0,0,0,0,1,1}};
+    for(int i=0;i<data.length;i++){
+      for(int j=0;j<data[i].length;j++){
+        System.out.print((data[i][j]==1)?" # ":" . ");
+      }
+      System.out.println();
+    }
+  }
+}
