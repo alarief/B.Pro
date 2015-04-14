0cce84f81eadc25c44986fccf77bcc434f630b7f
alarief <14410100061@stikom.edu>
2015-04-14 15:09:53 +0700

    Update Perkalian.java

--- Perkalian.java
+++ Perkalian.java
@@ -1,23 +1,20 @@
 import  java.util.Scanner;
-public class Perkalian {
+public class perkalian {
     public static void main(String[] args) {
+   
         Scanner input = new Scanner(System.in);
 
-        int i = 1, j = 1, hasil;
+    int i = 1, j = 1, hasil;
         String enter;
 
         for (i = 1; i <= 10; i++) {
             for (j = 1; j <= 10; j++) {
                 hasil = i * j;
-                System.out.println(i + "x" + j + '=' + hasil);
+                System.out.print(i + "x" + j + '=' + hasil);
+				 System.out.print("  ");
                 if (j == 10) {
-                    System.out.println("Enter");
-                    enter = input.nextLine();
-
-                    if (enter.equals("")) {
-                        continue;
-
-                    }
+                    System.out.println();
+                   
                 }
             }
         }
